8b5e897b44d96bc3440faad0a0a5f9f3ac3385a8
Temurbek Mirzaliev <mirzaliyevtemur@gmail.com>
2023-04-17 02:27:06 +0200

    fixed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import projImg3 from "../assets/img/mac.jpg";
 import projImg4 from "../assets/img/todo.jpg";
 import projImg5 from "../assets/img/tic.jpg";
 
+
 import projImg6 from "../assets/img/cart.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
